e415d24e7e7a4dcafc11c2bfcbad08d76309fdb2
Chu1503 <shivakash2003@gmail.com>
2024-01-26 16:39:44 +0530

    nextjs initialization

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Illumina_2.0_Website
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+  <div>
+    <Head>
+      <title>Illumina 2.0</title>
+    </Head>
+    
+    {/* Navbar */}
+
+    {/* HomePage */}
+
+    {/* About IEI */}
+
+    {/* Speakers */}
+
+    {/* Timeline */}
+
+    {/* Prizes */}
+
+    {/* Sponsors */}
+
+    {/* Footer */}
+    
+  </div>
+  )
+};
+
+export default Home;
