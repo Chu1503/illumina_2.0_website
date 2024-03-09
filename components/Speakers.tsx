bce3da6173ec8e2c9632333b983526bd16ba6bfd
Chu1503 <shivakash2003@gmail.com>
2024-03-10 00:59:27 +0530

    speaker card

--- components/Speakers.tsx
+++ components/Speakers.tsx
@@ -1,7 +1,8 @@
-import React , { useRef, useState }from "react";
+import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { FiMousePointer } from "react-icons/fi";
 import iei_logo from "../images/iei_logo.png";
+import TiltCard from "./TiltCard";
 
 type Props = {};
 
@@ -38,92 +39,9 @@ export default function Speakers({}: Props) {
         viewport={{ once: true }}
         className="flex flex-col space-y-10 px-0 md:px-10"
       >
-        <p className="text-lg sm:text-xl text-center leading-10 sm:leading-10 sm:pl-20 sm:pr-20">
-        </p>
+        <p className="text-lg sm:text-xl text-center leading-10 sm:leading-10 sm:pl-20 sm:pr-20"></p>
       </motion.div>
-        <TiltCard    />
+      <TiltCard />
     </motion.div>
-    
   );
-
 }
-
-
-const ROTATION_RANGE = 32.5;
-const HALF_ROTATION_RANGE = 32.5 / 2;
-
-const TiltCard = () => {
-  const ref = useRef(null);
-
-  const [rotateX, setRotateX] = useState(0);
-  const [rotateY, setRotateY] = useState(0);
-
-  const handleMouseMove = (e) => {
-    if (!ref.current) return;
-
-    const rect = ref.current.getBoundingClientRect();
-
-    const width = rect.width;
-    const height = rect.height;
-
-    const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
-    const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
-
-    const rY = mouseX / width - HALF_ROTATION_RANGE;
-    const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
-
-    setRotateX(rX);
-    setRotateY(rY);
-  };
-
-  const handleMouseLeave = () => {
-    if (!ref.current) return;
-    setRotateX(0);
-    setRotateY(0);
-  };
-
-  return (
-    
-    <motion.div
-      ref={ref}
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
-      style={{
-        transformStyle: "preserve-3d",
-      }}
-      animate={{
-        rotateX,
-        rotateY,
-      }}
-      className="relative h-96 w-72 rounded-xl bg-gradient-to-br from-indigo-300 to-violet-300"
-    >
-      
-      <div
-        style={{
-          transform: "translateZ(75px)",
-          transformStyle: "preserve-3d",
-        }}
-        className="absolute inset-4 grid place-content-center rounded-xl bg-white shadow-lg"
-      >
-        <FiMousePointer
-          style={{
-            transform: "translateZ(75px)",
-          }}
-          className="mx-auto text-4xl"
-        />
-        <p
-          style={{
-            transform: "translateZ(50px)",
-          }}
-          className="text-center text-2xl font-bold text-black "
-        >
-          <img src={iei_logo.src}></img>
-          <h1>Name Randoms</h1>
-        </p>
-      </div>
-      
-    </motion.div>
-  );
-};
-
-
---
+++ components/TiltCard.tsx
@@ -0,0 +1,89 @@
+import { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import iei_logo from "../images/iei_logo.png";
+import { FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+const ROTATION_RANGE = 32.5;
+const HALF_ROTATION_RANGE = 32.5 / 2;
+
+const TiltCard = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [rotateX, setRotateX] = useState<number>(0);
+  const [rotateY, setRotateY] = useState<number>(0);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!ref.current) return;
+
+    const rect = ref.current.getBoundingClientRect();
+
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
+    const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
+
+    const rY = mouseX / width - HALF_ROTATION_RANGE;
+    const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
+
+    setRotateX(rX);
+    setRotateY(rY);
+  };
+
+  const handleMouseLeave = () => {
+    if (!ref.current) return;
+    setRotateX(0);
+    setRotateY(0);
+  };
+
+  return (
+    <motion.div
+      ref={ref}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{
+        transform: "translateZ(500px)",
+        transformStyle: "preserve-3d",
+        backdropFilter: "blur(20px)",
+        backgroundColor: "rgba(255, 255, 255, 0.15)",
+      }}
+      animate={{
+        rotateX,
+        rotateY,
+      }}
+      className="relative h-auto w-auto rounded-xl flex sm:flex-row flex-col items-center p-5"
+    >
+      <div className="flex-shrink-0">
+        <img
+          className="sm:h-[20vw] sm:w-[20vw] h-[50vw] w-[50vw] rounded-full"
+          src={iei_logo.src}
+          alt="iei_logo"
+        />
+      </div>
+      <div className="flex flex-col flex-grow justify-center p-4">
+        <h1 className="text-5xl font-bold">Name</h1>
+        <p className="text-md mt-5 text-justify">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac
+          justo ut quam efficitur commodo.
+        </p>
+        <div className="flex justify-center items-center mt-5">
+          {" "}
+          <a href="#" className="align-center">
+            <FaInstagram
+              className="text-pink-500 hover:text-pink-700 mr-2.5"
+              size={25}
+            />
+          </a>
+          <a href="#" className="">
+            <FaLinkedin
+              className="text-blue-500 hover:text-blue-700 ml-2.5"
+              size={25}
+            />
+          </a>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default TiltCard;
