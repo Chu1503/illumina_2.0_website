69f07a14ef19eb6f77fbe6d7b461395b659c6923
Chu1503 <shivakash2003@gmail.com>
2025-07-20 17:41:55 +0530

    particle

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import illumina_logo_text from "../images/illumina_logo_text.png";
-import aurora from "../images/aurora.webp";
+import illumina_logo_text from "../images/illumina_logo_text.webp";
 import front1 from "../images/front1.webp";
 import front2 from "../images/front2.webp";
 import front3 from "../images/front3.webp";
@@ -10,10 +9,10 @@ import front4 from "../images/front4.webp";
 import front5 from "../images/front5.webp";
 import front6 from "../images/front6.webp";
 import trees from "../images/trees.webp";
+import aurora from "../images/aurora.webp";
 import { useRouter } from "next/navigation";
 
 const layers = [
-  // { src: aurora, name: "aurora" },
   { src: front6, name: "front6" },
   { src: front5, name: "front5" },
   { src: front4, name: "front4" },
@@ -36,53 +35,10 @@ export default function Hero({}: Props) {
   }, []);
 
   return (
-    <div className="relative h-screen w-full overflow-hidden">
-      {layers.map((layer, index) => {
-        const isTrees = layer.name === "trees";
-        const isFront1 = layer.name === "front1";
-        const isFront2 = layer.name === "front2";
-        const isFront3 = layer.name === "front3";
-        const isFront4 = layer.name === "front4";
-        const isFront5 = layer.name === "front5";
-        const isFront6 = layer.name === "front6";
-        const isAurora = layer.name === "aurora";
-
-        let transform = "none";
-
-        if (isFront6) {
-          transform = `translateY(${offsetY * -0.2}px)`;
-        } else if (isFront5) {
-          transform = `translateY(${offsetY * -0.4}px)`;
-        } else if (isFront4) {
-          transform = `translateY(${offsetY * -0.4}px)`;
-        } else if (isFront3) {
-          transform = `translateY(${offsetY * -0.1}px)`;
-        } else if (isFront2) {
-          transform = `translateY(${offsetY * -0.4}px)`;
-        } else if (isFront1) {
-          transform = `translateY(${offsetY * -0.3}px)`;
-        } else if (isTrees) {
-          transform = `translateY(${offsetY * -0.1}px)`;
-        } else if (isAurora) {
-          transform = `translateY(${offsetY * -0.05}px)`;
-        }
-
-        return (
-          <Image
-            key={index}
-            src={layer.src}
-            alt={`layer-${layer.name}`}
-            fill
-            className="object-cover absolute pointer-events-none"
-            style={{
-              zIndex: 10 + index,
-              transform,
-            }}
-          />
-        );
-      })}
-      <div
-        className="absolute inset-0 z-0 h-[60%]"
+    // <div className="relative h-screen w-full overflow-hidden">
+    <div className="relative h-screen w-full overflow-hidden ">
+      {/* <div
+        className="absolute inset-0 z-0 h-[69%]"
         style={{
           backgroundImage: `
       linear-gradient(115deg, rgba(130, 36, 104, 0.4) 0%, transparent 70%),
@@ -92,39 +48,91 @@ export default function Hero({}: Props) {
           backgroundSize: "400% 400%",
           animation: "aurora 60s linear infinite",
         }}
-      />
+      /> */}
+      <div className="particle-bg" />
+      {/* <Image
+        src={aurora}
+        alt="Aurora Background"
+        fill
+        priority
+        className="object-cover scale-120"
+      /> */}
 
-      <div
-        className="absolute left-1/2 top-[200px] flex flex-col items-center gap-2 md:gap-0"
-        style={{
-          transform: `translate(-50%, -50%) translateY(${offsetY * 0.6}px)`,
-          zIndex: 1,
-        }}
-      >
-        <img
-          className="relative rounded-full sm:w-[50vw] w-[100vw] mx-auto"
-          src={illumina_logo_text.src}
-          alt="ILLUMINA LOGO TEXT"
-        />
+      <div className="mt-[100px] relative w-full h-full">
+        {layers.map((layer, index) => {
+          const isTrees = layer.name === "trees";
+          const isFront1 = layer.name === "front1";
+          const isFront2 = layer.name === "front2";
+          const isFront3 = layer.name === "front3";
+          const isFront4 = layer.name === "front4";
+          const isFront5 = layer.name === "front5";
+          const isFront6 = layer.name === "front6";
 
-        <button
-          className="text-lg p-2 border border-white text-white hover:bg-white hover:text-black hover:border-black rounded-lg cursor-pointer transition-opacity duration-500"
+          let transform = "none";
+
+          if (isFront6) {
+            transform = `translateX(${offsetY * 0.6}px)`;
+          } else if (isFront5) {
+            transform = `translateY(${offsetY * 0.2}px)`;
+          } else if (isFront4) {
+            transform = `translateY(${offsetY * 0.1}px)`;
+          } else if (isFront3) {
+            transform = `translateX(${offsetY * -0.3}px)`;
+          } else if (isFront2) {
+            transform = `translateX(${offsetY * -0.3}px)`;
+          } else if (isFront1) {
+            transform = `translateX(${offsetY * -0.2}px)`;
+          } else if (isTrees) {
+            transform = `translateY(${offsetY * 0.1}px)`;
+          }
+
+          return (
+            <Image
+              key={index}
+              src={layer.src}
+              alt={`layer-${layer.name}`}
+              fill
+              className="object-cover absolute pointer-events-none"
+              style={{
+                zIndex: 10 + index,
+                transform,
+              }}
+            />
+          );
+        })}
+
+        <div
+          className="absolute left-1/2 top-[150px] flex flex-col items-center gap-2 md:gap-0"
           style={{
-            opacity:
-              typeof window !== "undefined" &&
-              offsetY > window.innerHeight * 0.1
-                ? 0
-                : 1,
-            pointerEvents:
-              typeof window !== "undefined" &&
-              offsetY > window.innerHeight * 0.1
-                ? "none"
-                : "auto",
+            transform: `translate(-50%, -50%) translateY(${offsetY * 0.6}px)`,
+            zIndex: 1,
           }}
-          onClick={() => router.push("/login")}
         >
-          Get Started
-        </button>
+          <img
+            className="relative rounded-full sm:w-[50vw] w-[100vw] mx-auto"
+            src={illumina_logo_text.src}
+            alt="ILLUMINA LOGO TEXT"
+          />
+
+          <button
+            className="text-lg p-2 border border-white text-white hover:bg-white hover:text-black hover:border-black rounded-lg cursor-pointer transition-opacity duration-500"
+            style={{
+              opacity:
+                typeof window !== "undefined" &&
+                offsetY > window.innerHeight * 0.1
+                  ? 0
+                  : 1,
+              pointerEvents:
+                typeof window !== "undefined" &&
+                offsetY > window.innerHeight * 0.1
+                  ? "none"
+                  : "auto",
+            }}
+            onClick={() => router.push("/login")}
+          >
+            Get Started
+          </button>
+        </div>
       </div>
     </div>
   );
